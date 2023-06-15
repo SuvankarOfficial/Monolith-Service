75b1d8d551cc32f0fc8c0a10f2f4ddbce32aa7f3
Shiv(Workspace) <shivatneosoft@gmail.com>
2023-06-15 17:52:24 +0530

    Removed some ugly codes

--- src/main/java/com/contact/service/ContactService.java
+++ src/main/java/com/contact/service/ContactService.java
@@ -2,7 +2,6 @@ package com.contact.service;
 
 import com.contact.ResponseBean.ResponseBean;
 import com.contact.dto.RequestBean.ContactRequestBean;
-import com.contact.dto.ResponseBean.ContactResponseBean;
 import com.contact.entity.Contact;
 import com.contact.enums.ContactStatus;
 import com.contact.mapper.ContactMapper;
@@ -27,49 +26,48 @@ public class ContactService {
     public ResponseBean addContact(ContactRequestBean contactRequestBean){
         Optional<Contact> existingContact = contactRepository.findByContactName(contactRequestBean.getContactName());
         if(existingContact.isEmpty()) {
-            existingContact = Optional.ofNullable(contactMapper.requestEntityMapperCreate(contactRequestBean));
-            existingContact.get().setStatus(ContactStatus.Active);
-            existingContact = Optional.ofNullable(contactRepository.saveAndFlush(existingContact.get()));
-            return ResponseBean.builder().message("Contact is saved").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(existingContact.get())).build();
+            Contact contact = contactMapper.requestEntityMapperCreate(contactRequestBean);
+            contact = existingContact.get();
+            contact.setStatus(ContactStatus.Active);
+            contact = contactRepository.saveAndFlush(contact);
+            return ResponseBean.builder().message("Contact is saved").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(contact)).build();
         }
         else
             return ResponseBean.builder().message("Contact Name Already exist").status(Boolean.TRUE).build();
     }
 
     public ResponseBean findAllContact(){
-        List<Contact> existing = contactRepository.findAll();
-        if(existing.isEmpty()) {
+        List<Contact> contacts = contactRepository.findAll();
+        if(contacts.isEmpty())
             return ResponseBean.builder().status(Boolean.TRUE).message("No data Found").build();
-        }
-        return ResponseBean.builder().status(Boolean.TRUE).data(existing.stream().map(data-> contactMapper.entityResponseMapper(data))).build();
+
+        return ResponseBean.builder().status(Boolean.TRUE).data(contacts.stream().map(data-> contactMapper.entityResponseMapper(data))).build();
     }
 
     public ResponseBean findContactById(Long contactId){
-        Optional<Contact> existing = contactRepository.findById(contactId);
-        if(existing.isPresent())
-            return ResponseBean.builder().status(Boolean.TRUE).data(contactMapper.entityResponseMapper(existing.get())).build();
+        Optional<Contact> contact = contactRepository.findById(contactId);
+        if(contact.isPresent())
+            return ResponseBean.builder().status(Boolean.TRUE).data(contactMapper.entityResponseMapper(contact.get())).build();
         return ResponseBean.builder().status(Boolean.TRUE).message("No data Found").build();
     }
 
-    public ResponseBean updateContact(ContactRequestBean contact){
-        Optional<Contact> existing = contactRepository.findById(contact.getContactId());
-        if(existing.isEmpty())
+    public ResponseBean updateContact(ContactRequestBean contactRequestBean){
+        Optional<Contact> optionalContact = contactRepository.findById(contactRequestBean.getContactId());
+        if(optionalContact.isEmpty())
             return ResponseBean.builder().message("No Contact found to update").status(Boolean.TRUE).build();
-        else {
-            existing = Optional.ofNullable(contactMapper.requestEntityMapperUpdate(contact,existing.get()));
-            existing = Optional.ofNullable(contactRepository.saveAndFlush(existing.get()));
-            return ResponseBean.builder().message("Contact has been updated Successfully").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(existing.get())).build();
-        }
+        Contact contact = contactMapper.requestEntityMapperUpdate(contactRequestBean, optionalContact.get());
+        contact = contactRepository.saveAndFlush(contact);
+        return ResponseBean.builder().message("Contact has been updated Successfully").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(contact)).build();
     }
 
     public ResponseBean deleteContact(Long contactId){
-        Optional<Contact> existing = contactRepository.findById(contactId);
-        if(existing.isEmpty())
+        Optional<Contact> optionalContact = contactRepository.findById(contactId);
+        if(optionalContact.isEmpty())
             return ResponseBean.builder().message("No Contact found to delete").status(Boolean.TRUE).build();
-        else
-            existing.get().setStatus(ContactStatus.Deleted);
-            existing = Optional.ofNullable(contactRepository.saveAndFlush(existing.get()));
-            return ResponseBean.builder().message("Contact has been Deleted Successfull").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(existing.get())).build();
+        Contact contact = optionalContact.get();
+        contact.setStatus(ContactStatus.Deleted);
+        contact = contactRepository.saveAndFlush(contact);
+        return ResponseBean.builder().message("Contact has been Deleted Successfull").status(Boolean.TRUE).data(contactMapper.entityResponseMapper(contact)).build();
     }
 
 }
